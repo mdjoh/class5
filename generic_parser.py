1fb5555a9233efccaefb9c4ee7984131cbed04d3
Marchiano Dong Jun Oh <m_dongju@cce-fb105-19m.concordia.ca>
2019-02-16 10:55:57 -0500

    added new python and data files

---
+++ generic_parser.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# 1. load a dataset from a file
+# 2. "organize" that file, so we can access columns or rows of it easily
+# 3. Compute some "summary stats" about the dataset
+# 4. print those summary stats
+
+# 1. load a dataset
+# 1a. accept arbitrary filename as argument
+# 1b. load the file
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = 'A CSV reader + stats maker')
+parser.add_argument ('csvfile',
+		type=str,
+		help='Path to the input csv file.')
+
+parsed_args = parser.parse_args()
+# sys arg v is not recommended
+
+#print(parsed_args)
+#print(parsed_args.csvfile)
+
+my_csv_file = parsed_args.csvfile
+
+# check if the file being passed exists
+
+import os
+import os.path as op
+
+#solution 1
+#if os.path.isfile(my_csv_file):
+#	print("file is real")
+#else:
+#	print("not a file")
+
+# stop program running and display a message when an error is encountered 
+
+# original solution:
+assert op.isfile(my_csv_file), "give real file please"
+
+#solution 3
+#if not os.path.isfile(my_csv_file):
+#	raise ValueError("not a file. give real file")
+
+print('file is there!')
+
+
+# Part 1b. load the file
+import pandas as pd
+
+#piped separator; this case its 1 or more spaces or comma
+data = pd.read_csv(my_csv_file, sep='\s+|,' ,header=None)
+print(data.head())
+
+#display dir items for data
+#for item in dir(data):
+#	print(item)
+
+print(data.shape)
+
